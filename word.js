1a55cbc10f1713ac958da2e7ce833e50e8a9daae
NimraTalha <nimratalha@gmail.com>
2024-04-18 20:30:53 +0500

    message

---
+++ word.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+(async () => {
+    // Declare a constant "answers" and assign it to the result of inquirer.prompt function
+    const answers = await inquirer.prompt([
+        {
+            name: "Sentence",
+            type: "input",
+            message: "Enter a sentence to count the word"
+        }
+    ]);
+    const words = answers.Sentence.trim().split(" ");
+    console.log(words);
+    console.log(chalk.yellow(`Your sentence words count is : ${words.length}`));
+})();
